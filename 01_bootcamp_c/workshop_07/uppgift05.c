ba61ca4e000c9376a4bf1030600a653b184ec874
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-06-10 18:15:33 +0200

    feat: change scanf to fgets and enhance input validation

--- 01_bootcamp_c/workshop_07/uppgift05.c
+++ 01_bootcamp_c/workshop_07/uppgift05.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 #define CELSIUS 0x63
 #define FAHRENHEIT 0x66
 #define KELVIN 0x6B
+#define YES 0x79
+#define NO 0x6E
 #define BUFFER_SIZE 100
 
 int main() {
@@ -20,30 +23,48 @@ int main() {
             printf("~*");
         }
         printf("\nTemperature converter");
-        do
+        while (1)
         {
             printf("\nF/f=Fahrenheit\nC/c=Celsius\nK/k=Kelvin\nEnter temperature scale to convert from: ");
-            fgets(convert_scale_from, sizeof(convert_scale_from), stdin);
-            printf("%c", convert_scale_from);
-            // scanf(" %c", &convert_scale_from);
-            convert_scale_from |= 0x20;
-            if (convert_scale_from != CELSIUS && convert_scale_from != FAHRENHEIT && convert_scale_from != KELVIN) {
-                printf("Invalid character. Try again");
+            if (fgets(input_buffer, sizeof(input_buffer), stdin)) {
+                if (strlen(input_buffer) == 2 && (input_buffer[1]) == '\n' || (input_buffer[1]) == '\r') {
+                    convert_scale_from = input_buffer[0] | 0x20;
+                    // convert_scale_from |= 0x20;
+                    if (convert_scale_from == CELSIUS || convert_scale_from == FAHRENHEIT || convert_scale_from == KELVIN) {
+                        break;
+                    }
+                }
             }
-        } while (convert_scale_from != CELSIUS && convert_scale_from != FAHRENHEIT && convert_scale_from != KELVIN);
+            printf("Invalid character. Try again");
+        }
 
-        do
+        while (1)
         {
             printf("\nEnter temperature scale to convert to: ");
-            scanf(" %c", &convert_scale_to);
-            convert_scale_to |= 0x20;
-            if (convert_scale_to != CELSIUS && convert_scale_to != FAHRENHEIT && convert_scale_to != KELVIN) {
-                printf("Invalid character. Try again");
+            if (fgets(input_buffer, sizeof(input_buffer), stdin)) {
+                if (strlen(input_buffer) == 2 && (input_buffer[1] == '\n') || (input_buffer[1] == '\r')) {
+                    convert_scale_to = input_buffer[0] | 0x20;
+                    // convert_scale_to |= 0x20;
+                    if (convert_scale_to == CELSIUS || convert_scale_to == FAHRENHEIT || convert_scale_to == KELVIN) {
+                        break;
+                    }
+                }
             }
-        } while (convert_scale_to != CELSIUS && convert_scale_to != FAHRENHEIT && convert_scale_to != KELVIN);
+            printf("Invalid character. Try again");
+        }
 
-        printf("Enter a number to convert: ");
-        scanf("%lf", &temp_number);
+        while (1) {
+            char* end_ptr;
+            printf("Enter a number to convert: ");
+            // scanf("%lf", &temp_number);
+            if (fgets(input_buffer, sizeof(input_buffer), stdin)) {
+                temp_number = strtod(input_buffer, &end_ptr);
+                if (end_ptr != input_buffer && (*end_ptr == '\n' || *end_ptr == '\0' || isspace(*end_ptr))) {
+                    break;
+                }
+            }
+            printf("Invalid number. Try again.");
+        }
         switch (convert_scale_from) {
         case 'f':
             switch (convert_scale_to) {
@@ -84,10 +105,22 @@ int main() {
         for (int j = 0; j < 20; ++j) {
             printf("~*");
         }
-        printf("\nDo you want to convert more temperatures?\nY/N?: ");
-        scanf(" %c", &new_convert);
-        new_convert |= 0x20;
-        if (new_convert == 0x6E) {
+        while (1) {
+            printf("\nDo you want to convert more temperatures?\nY/N?: ");
+            // scanf(" %c", &new_convert);
+            if (fgets(input_buffer, sizeof(input_buffer), stdin)) {
+                if (strlen(input_buffer) == 2 && (input_buffer[1] == '\n' || (input_buffer[1] == '\r'))) {
+                    // new_convert |= 0x20;
+                    new_convert = input_buffer[0] | 0x20;
+                    if (new_convert == YES || new_convert == NO) {
+                        break;
+                    }
+                }
+            }
+            printf("Invalid input. Enter Y/y or N/n\n");
+        }
+
+        if (new_convert == NO) {
             break;
         }
         temp_number = 0.0;
